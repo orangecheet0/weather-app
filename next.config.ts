ab3c461a9d7248199a56243c2075162b3171577e
orangecheet0 <alweatherpage@gmail.com>
2025-08-12 18:45:40 -0500

    Enable lint checks during build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Allow production builds to complete even if there are ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     domains: ['openweathermap.org'], // Allow images from this domain
   },
